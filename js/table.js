c1db1723405d89233754967920ef5a5f46c7822e
Bodigrim <andrew.lelechenko@gmail.com>
2016-01-24 15:57:37 +0200

    #2 Fix sort by status

--- js/table.js
+++ js/table.js
@@ -5,6 +5,7 @@ $(function(){
 			email: $(this).attr("data-email"),
 			status: $(this).val()
 		};
+		$(this).parents("td").attr("data-value", $(this).val());
 		$.post("/actions/update-status.php", hash);
 	});
 
