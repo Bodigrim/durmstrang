694a98a2113db3500ed76b54840ac56c6a8689cc
Bodigrim <andrew.lelechenko@gmail.com>
2016-10-02 01:22:03 +0300

    Refactor backup action

--- actions/backup.php
+++ actions/backup.php
@@ -2,18 +2,42 @@
 
 include "../include/config.php";
 
+function sendSqlHeaders($filename){
+  if(headers_sent())
+    throw new Exception("sendSqlHeaders: headers has been already sent");
+
+  header("Content-Type: application/sql");
+  header("Content-Disposition: attachment;filename=\"$filename\"");
+  header('Cache-Control: max-age=0');
+}
+
+function commandToDumpTables($tableNames){
+  if(!count($tableNames))
+    throw new Exception("dumpTables: empty list of tables");
+
+  $prependPref = function($s){return PREF . $s;};
+
+  $command = "mysqldump --compact"
+           . " --user=" . DB_USER
+           . " --password=" . DB_PASS
+           . " " . DB_NAME
+           . " " . implode(" ", array_map($prependPref, $tableNames));
+
+  return $command;
+}
+
 $editorid = loginbycookie();
 if(!isAdmin($editorid))
 	die("У вас недостаточно прав доступа, чтобы скачать бекап. ");
 
-$filename = "durmstrang.sql";
+sendSqlHeaders(PROJECT_NAME . ".sql");
 
-header("Content-Type: application/sql");
-header("Content-Disposition: attachment;filename=\"$filename\"");
-header('Cache-Control: max-age=0');
-
-$command = "mysqldump --compact --user=" . DB_USER . " --password=" . DB_PASS . " " . DB_NAME
-					. " ".PREF."sessions ".PREF."users ".PREF."config ".PREF."messages";
-passthru($command);
+$tableNames =
+  [ "sessions"
+  , "users"
+  , "config"
+  , "messages"
+  ];
+passthru(commandToDumpTables($tableNames));
 
 ?>
