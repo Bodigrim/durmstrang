85160831c63dec6a509cad0ef95280b7644fc59a
Bodigrim <andrew.lelechenko@gmail.com>
2018-04-22 18:02:17 +0000

    Improve contraindications page

--- contraindications.php
+++ contraindications.php
@@ -7,14 +7,30 @@ if(!isAdmin($editorid))
 	die("У вас недостаточно прав доступа, чтобы просматривать список заявок. Вернитесь на <a href=\"/\">главную страницу</a>.");
 
 $sql = "SELECT u.surname, u.name, u.contraindication
-	FROM ".PREF."users AS u
-	WHERE u.active=1 AND u.surname<>'' AND u.contraindication<>''
-	ORDER BY u.surname ASC, u.name ASC";
+  FROM ".PREF."users AS u
+  WHERE u.active=1
+    AND u.surname<>''
+    AND u.contraindication<>''
+    AND u.email NOT LIKE '%@example.com'
+    AND u.status IN ('query', 'participant')
+  ORDER BY u.surname ASC, u.name ASC";
 $result = query($sql);
 $userData = fetch_assocs($result);
 
+$sql = "SELECT u.email, u.surname, u.name
+  FROM ".PREF."users AS u
+  WHERE u.active=1
+    AND u.surname<>''
+    AND u.contraindication=''
+    AND u.email NOT LIKE '%@example.com'
+    AND u.status IN ('query', 'participant')
+  ORDER BY u.surname ASC, u.name ASC";
+$result = query($sql);
+$missingContraindications = fetch_assocs($result);
+
 $render_data = [
-	"users"       => $userData,
+  "users"   => $userData,
+  "missing" => $missingContraindications,
   ];
 
 $ret = constructTwig()->render("contraindications.twig", $render_data);
